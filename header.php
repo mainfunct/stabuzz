0922ed150038f5e0771610357c9975dbfd224b2d
김다연 <kimhj9725@naver.com>
2021-07-29 15:14:49 +0900

    complete stabuzz

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>스타버즈 커피</title>
+        <link rel="stylesheet" href="starbuzz.css">
+    </head>
+    <body>
+        <header class="top">
+            <img id="headerLogo" 
+                src="images/headerLogo.gif" alt="Starbuzz Coffeee logo image">
+            <img id="headerSlogan" 
+                src="images/headerSlogan.gif" 
+                alt="Providing all the caffeine you need to power your life.">
+        </header>
+        <ul>
+            <li><a href="index.php">홈</a></li>
+            <li><a href="blog.php">블로그</a></li>
+            <li><a href="">신제품</a></li>
+            <li><a href="">조리법</a></li>
+            <li><a href="">위치</a></li>
+            <li><a href="">공지사항</a></li>
+            <li><a href="">회사소개</a></li>
+            <li><a href="">자유게시판</a></li>
+        </ul>
